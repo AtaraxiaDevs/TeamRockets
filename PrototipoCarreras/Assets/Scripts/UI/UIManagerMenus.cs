e64834ec968ebd28ddcdbff38354cfcb2f312c89
MeSientoYMeCallo99 <72436411+MeSientoYMeCallo99@users.noreply.github.com>
2020-12-31 09:47:06 +0100

    Hola soy Interfaz

--- PrototipoCarreras/Assets/Scripts/UI/UIManagerMenus.cs
+++ PrototipoCarreras/Assets/Scripts/UI/UIManagerMenus.cs
@@ -12,15 +12,82 @@ public class UIManagerMenus : MonoBehaviour
     [Header("Pestañas")]
     public GameObject pestanaActual;
 
+    [Header("Titulo")]
+    public bool esTitulo;
+    public Image titulo;
+    public Sprite spriteEspañol;
+    public Sprite spriteIngles;
+    public Sprite spriteGallego;
+
+    [Header("OtroTitulo")]
+    public bool esTitulo2;
+    public Image titulo2;
+    public Sprite spriteEspañol2;
+    public Sprite spriteIngles2;
+    public Sprite spriteGallego2;
+
+    private void Update()
+    {
+        if (esTitulo)
+        {
+            if(InformacionPersistente.singleton.idiomaActual == 0)
+            {
+                titulo.sprite = spriteEspañol;
+
+                if (esTitulo2)
+                {
+                    titulo2.sprite = spriteEspañol2;
+                }
+            }
+            else if (InformacionPersistente.singleton.idiomaActual == 1)
+            {
+                titulo.sprite = spriteIngles;
+
+                if (esTitulo2)
+                {
+                    titulo2.sprite = spriteIngles2;
+                }
+            }
+            else if (InformacionPersistente.singleton.idiomaActual == 2)
+            {
+                titulo.sprite = spriteGallego;
+
+                if (esTitulo2)
+                {
+                    titulo2.sprite = spriteGallego2;
+                }
+            }
+            else
+            {
+                titulo.sprite = spriteEspañol;
+
+                if (esTitulo2)
+                {
+                    titulo2.sprite = spriteEspañol2;
+                }
+            }
+        }
+    }
+
     public void IrA(string s)
     {
         sceneFader.FadeTo(s);
+        InformacionPersistente.singleton.escenaActual = s;
     }
 
     public void CambiarPanel(GameObject scene)
     {
         scene.SetActive(!scene.activeSelf);
         PPrincipal.SetActive(!PPrincipal.activeSelf);
+
+        if (PPrincipal.activeSelf)
+        {
+            InformacionPersistente.singleton.escenaActual = PPrincipal.name;
+        }
+        else
+        {
+            InformacionPersistente.singleton.escenaActual = scene.name;
+        }
     }
 
     public void CambiarPestana(GameObject scene)
--- PrototipoCarreras/Assets/UI/UIManagerButtons.cs
+++ PrototipoCarreras/Assets/UI/UIManagerButtons.cs
@@ -7,19 +7,34 @@ using UnityEngine.UI;
 
 public class UIManagerButtons : MonoBehaviour
 {
-    private string jsonData = "";
+    private string jsonData = "", buttText = "";
     public string buttKey = "menuPrincipal";
+
     public Text hola;
-    private string buttText = "";
+
+    private int idioma;
+
     void Start()
     {
         jsonData = File.ReadAllText(Application.dataPath + "/UI/localization.json");
         SimpleJSON.JSONNode data = SimpleJSON.JSON.Parse(jsonData);
-        buttText = data[InformacionPersistente.singleton.escenaActual][buttKey][InformacionPersistente.singleton.idiomaActual].Value;
-        Debug.Log(InformacionPersistente.singleton.idiomaActual);
+
+        idioma = InformacionPersistente.singleton.idiomaActual;
+
+        buttText = data[InformacionPersistente.singleton.escenaActual][buttKey][idioma].Value;
         hola.text = buttText;
     }
 
     // Update is called once per frame
-
+    private void Update()
+    {
+        if(idioma != InformacionPersistente.singleton.idiomaActual)
+        {
+            jsonData = File.ReadAllText(Application.dataPath + "/UI/localization.json");
+            SimpleJSON.JSONNode data = SimpleJSON.JSON.Parse(jsonData);
+            idioma = InformacionPersistente.singleton.idiomaActual;
+            buttText = data[InformacionPersistente.singleton.escenaActual][buttKey][idioma].Value;
+            hola.text = buttText;
+        }
+    }
 }
